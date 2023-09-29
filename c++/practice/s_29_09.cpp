61851494329e75b8aa891ec9ba1ea5141b2ec215
nilesh <nileshkumarb147@gmail.com>
2023-09-29 20:04:38 +0530

    29_09_2023

---
+++ c++/practice/s_29_09.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+/*
+step - 1 : create file name d_date_month in pracctice folder
+step - 2 : write header file and first print day and date
+step - 3 : code ...
+step - 4 : push repo on github
+
+ >> steps for github action
+ step - 1 : check path 
+ step - 2 : write commmand in cmd {try with bash}
+ step - 4 : commands are - 
+            git add .
+            git commit -m "message"
+            git push
+
+and then send repo link to my whatsapp
+
+
+*/
+
+int main(){
+    cout<<"day - 1 29-09-20023\n";
+    // start code here...
+
+    return 0;
+}
---
+++ c++/project2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // if=marks;
+    int marks = 0;
+    cout<<"enter your marks";
+    cin>>marks;
+
+    if(marks>=33){
+        // cout<<varibalename | value
+        cout<<"passed";
+    }else {
+
+    }
+
+}
